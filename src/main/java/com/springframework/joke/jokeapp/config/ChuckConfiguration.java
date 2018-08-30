6a40b1c6bf2c1ec316ca11681f894feea6942e51
Ehtiram Abdullayev <ehtiramabdullayev@gmail.com>
2018-08-30 14:24:33 +0200

    added configuration class and bean

---
+++ src/main/java/com/springframework/joke/jokeapp/config/ChuckConfiguration.java
@@ -0,0 +1,19 @@
+package com.springframework.joke.jokeapp.config;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Ehtiram_Abdullayev on 8/30/2018
+ * @project jokeapp
+ */
+@Configuration
+public class ChuckConfiguration {
+
+    @Bean
+    public ChuckNorrisQuotes chuckNorrisQuotes(){
+
+        return new ChuckNorrisQuotes();
+    }
+}
