b0c2b5b116c9287bfa4c6e54789e4d96b54a435d
Ehtiram Abdullayev <ehtiramabdullayev@gmail.com>
2018-09-07 15:02:18 +0200

    removed unused imports

--- src/main/java/com/springframework/joke/jokeapp/config/ChuckConfiguration.java
+++ src/main/java/com/springframework/joke/jokeapp/config/ChuckConfiguration.java
@@ -1,8 +1,6 @@
 package com.springframework.joke.jokeapp.config;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 /**
  * @author Ehtiram_Abdullayev on 8/30/2018
